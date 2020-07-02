f86538c08887e3d4d5a8a0200f2eb4acfb975b7f
filipem2210 <47154367+filipem2210@users.noreply.github.com>
2020-07-02 01:16:43 -0300

    Sign up implemented

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -5,15 +5,13 @@ import { connect } from 'react-redux';
 import { signIn } from './SignInActions';
 
 const SignIn = (props) => {
-  const { account, signIn } = props;
+  const { signIn } = props;
 
   const submitHandler = (e) => {
     e.preventDefault();
 
     signIn({ email: 'filipexzw1@gmail.com', password: '123456' });
   }
-
-  console.log('*** SignIn.account', account);
 
   return (
     <div className="container h-100 pt-5">
